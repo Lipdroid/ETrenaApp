2d65a96a12ec98cd6a4b1d1f2b62dabe9891eed2
lipuhossain <kutu1234>
2018-06-10 02:33:39 +0600

    some codes in editprofile page

--- app/src/main/java/com/dtmweb/etrendapp/fragments/EditProfileFragment.java
+++ app/src/main/java/com/dtmweb/etrendapp/fragments/EditProfileFragment.java
@@ -6,15 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.dtmweb.etrendapp.R;
+import com.dtmweb.etrendapp.constants.Constants;
+import com.dtmweb.etrendapp.customViews.CircleImageView;
+import com.dtmweb.etrendapp.models.StoreObject;
+import com.dtmweb.etrendapp.models.UserObject;
+import com.dtmweb.etrendapp.utils.GlobalUtils;
 import com.dtmweb.etrendapp.utils.MultipleScreen;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EditProfileFragment extends Fragment {
 
+
+    private UserObject mUserObj = null;
 
     public EditProfileFragment() {
         // Required empty public constructor
@@ -25,7 +39,112 @@ public class EditProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        View root = null;
+
+        switch (GlobalUtils.user_type) {
+            case Constants.CATEGORY_BUYER:
+                root = inflater.inflate(R.layout.activity_buyer_registration, container, false);
+                ImageView btn_cross = (ImageView) root.findViewById(R.id.btn_cross);
+                ImageView btn_image_selection = (ImageView) root.findViewById(R.id.btn_image_selection);
+                Button btn_go = (Button) root.findViewById(R.id.btn_go);
+                RelativeLayout header = (RelativeLayout) root.findViewById(R.id.header);
+                EditText et_full_name = (EditText) root.findViewById(R.id.et_full_name);
+                EditText et_password = (EditText) root.findViewById(R.id.et_password);
+                EditText et_password_retype = (EditText) root.findViewById(R.id.et_password_retype);
+                EditText et_country = (EditText) root.findViewById(R.id.et_country);
+                EditText et_city = (EditText) root.findViewById(R.id.et_city);
+                EditText et_address = (EditText) root.findViewById(R.id.et_address);
+                EditText et_contact = (EditText) root.findViewById(R.id.et_contact);
+                EditText et_user_name = (EditText) root.findViewById(R.id.et_user_name);
+                EditText et_mail = (EditText) root.findViewById(R.id.et_mail);
+
+                //set up the views
+                mUserObj = GlobalUtils.getCurrentUser();
+                if (mUserObj != null) {
+                    //hide the password field
+                    et_password_retype.setVisibility(View.GONE);
+                    et_password.setVisibility(View.GONE);
+                    btn_cross.setVisibility(View.GONE);
+                    header.setVisibility(View.GONE);
+                    //set the valuse to the view
+                    et_full_name.setText(mUserObj.getUsername());
+                    et_address.setText(mUserObj.getAddress());
+                    et_city.setText(mUserObj.getCity());
+                    et_country.setText(mUserObj.getCountry());
+                    et_mail.setText(mUserObj.getEmail());
+                    et_contact.setText(mUserObj.getContact_no());
+                    et_user_name.setText(mUserObj.getUsername());
+
+                    btn_go.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //update the buyer
+                        }
+                    });
+                }
+                break;
+            case Constants.CATEGORY_SELLER:
+                root = inflater.inflate(R.layout.activity_seller_registration, container, false);
+                btn_cross = (ImageView) root.findViewById(R.id.btn_cross);
+                header = (RelativeLayout) root.findViewById(R.id.header);
+                btn_image_selection = (CircleImageView) root.findViewById(R.id.btn_image_selection);
+                btn_go = (Button) root.findViewById(R.id.btn_go);
+                EditText et_store_name = (EditText) root.findViewById(R.id.et_store_name);
+                et_password = (EditText) root.findViewById(R.id.et_password);
+                et_password_retype = (EditText) root.findViewById(R.id.et_password_retype);
+                EditText et_bank_name = (EditText) root.findViewById(R.id.et_bank_name);
+                EditText et_bank_account_name = (EditText) root.findViewById(R.id.et_bank_account_name);
+                EditText et_bank_account_number = (EditText) root.findViewById(R.id.et_bank_account_number);
+                et_country = (EditText) root.findViewById(R.id.et_country);
+                et_city = (EditText) root.findViewById(R.id.et_city);
+                et_address = (EditText) root.findViewById(R.id.et_address);
+                EditText et_store_owner_name = (EditText) root.findViewById(R.id.et_store_owner_name);
+                EditText et_store_owner_contact = (EditText) root.findViewById(R.id.et_store_owner_contact);
+                et_mail = (EditText) root.findViewById(R.id.et_mail);
+                EditText et_instagram = (EditText) root.findViewById(R.id.et_instagram);
+
+                //set up the views
+                mUserObj = GlobalUtils.getCurrentUser();
+                if (mUserObj != null) {
+                    //hide the password field
+                    et_password_retype.setVisibility(View.GONE);
+                    et_password.setVisibility(View.GONE);
+                    btn_cross.setVisibility(View.GONE);
+                    header.setVisibility(View.GONE);
+                    //set the valuse to the view
+                    et_address.setText(mUserObj.getAddress());
+                    et_city.setText(mUserObj.getCity());
+                    et_country.setText(mUserObj.getCountry());
+                    et_mail.setText(mUserObj.getEmail());
+                    et_store_owner_contact.setText(mUserObj.getContact_no());
+                    et_store_owner_name.setText(mUserObj.getUsername());
+
+                    StoreObject mStoreObj = mUserObj.getStoreObject();
+                    if (mStoreObj != null) {
+                        et_bank_account_name.setText(mStoreObj.getBank_acc_name());
+                        et_bank_account_number.setText(mStoreObj.getBank_acc_number());
+                        et_bank_name.setText(mStoreObj.getBank_name());
+
+                    }
+
+                    Picasso.get()
+                            .load(mUserObj.getPro_img())
+                            .placeholder(R.color.common_gray)
+                            .error(R.color.common_gray)
+                            .into(btn_image_selection);
+                }
+                btn_go.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //update the seller
+                    }
+                });
+                break;
+            case Constants.CATEGORY_NON_LOGGED:
+                root = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+                break;
+        }
+
         new MultipleScreen(getActivity());
         MultipleScreen.resizeAllView((ViewGroup) root);
         return root;
